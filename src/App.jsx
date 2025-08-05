13eea163110e2f33c3599e45c33bbb1f4daf7a03
DEVID19 <devidbisen84@gmail.com>
2025-08-05 23:55:23 +0530

    minmal changes done

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
 
 const App = () => {
   return (
+    //Router setup
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
